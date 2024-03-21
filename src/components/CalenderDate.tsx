46bf0e2e99f4b38936d92b2519ef59ad16785c64
Ewy Rostam <ewy.rost@gmail.com>
2024-03-21 22:51:51 +0100

    Now it's not updating on save either

--- src/components/CalenderDate.tsx
+++ src/components/CalenderDate.tsx
@@ -1,10 +1,10 @@
 type Props = {
     date: number;
     color: string;
-    onDateClick: (date: number) => void;
+    onDateClick: () => void;
 }
 export default function CalenderDate({date, color, onDateClick}: Props){
     return(
-    <button onClick={() => onDateClick(date)} className={`w-1/7 border border-black hover:border-white ${color}`}>{date}</button>
+    <button onClick={onDateClick} className={`w-1/7 border border-black hover:border-white ${color}`}>{date}</button>
     )
 }
