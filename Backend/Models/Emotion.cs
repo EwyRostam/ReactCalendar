4ba85eaad1c0872a983ae1d980b10713d0e0685e
Ewy Rostam <ewy.rost@gmail.com>
2024-04-06 09:29:03 +0200

    Set up emotion model

--- Backend/Models/Emotion.cs
+++ Backend/Models/Emotion.cs
@@ -2,6 +2,12 @@ namespace Backend.Models
 {
     public class Emotion
     {
-        
+        public int Id { get; set; }
+        public required string Content { get; set; }
+        public int Value {get; set;}
+        public bool IsWanted {get; set;}
+        public int OppositeId { get; set; }
+        public required Emotion Opposite { get; set; }
+
     }
 }
