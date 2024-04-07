84e9f64a82fadffbd4ed87288bbd6cd5cd9840f4
Ewy Rostam <ewy.rost@gmail.com>
2024-04-07 11:00:42 +0200

    Make day and emotion in DayEmotion required

--- Backend/Models/DayEmotion.cs
+++ Backend/Models/DayEmotion.cs
@@ -3,8 +3,8 @@ namespace Backend.Models
     public class DayEmotion
     {
         public int DayId {get; set;}
+        public required Day Day {get; set;}
         public int EmotionId {get; set;}
-        public Day? Day {get; set;}
-        public Emotion? Emotion {get; set;}
+        public required Emotion Emotion {get; set;}
     }
 }
