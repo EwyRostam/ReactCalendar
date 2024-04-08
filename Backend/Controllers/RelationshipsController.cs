a8739a588a495475ada7e54e6effe8d46dd14879
Ewy Rostam <ewy.rost@gmail.com>
2024-04-08 21:03:13 +0200

    Add endpoints to post, getspecific and get all for relationships

--- Backend/Controllers/RelationshipsController.cs
+++ Backend/Controllers/RelationshipsController.cs
@@ -1,4 +1,7 @@
+using Backend.Data;
+using Backend.Models.Enteties;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers
 {
@@ -6,6 +9,39 @@ namespace Backend.Controllers
     [Route("[controller]")]
     public class RelationshipsController : ControllerBase
     {
-        
+         private readonly AppDBContext _context;
+
+        public RelationshipsController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Relationship>> CreateRelationship(Relationship relationship)
+        {
+            if(await _context.Relationships.AnyAsync(rel => rel.Name == relationship.Name))
+            {
+                return BadRequest();
+            }
+
+            await _context.Relationships.AddAsync(relationship);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetRelationship), new { id = relationship.Id }, relationship);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Relationship>> GetRelationship(int id)
+        {
+            var relationship = await _context.Relationships.FirstOrDefaultAsync(relationship => relationship.Id == id);
+            return relationship == null ? NoContent() : relationship;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Relationship>>> GetAllRelationships()
+        {
+           return await _context.Relationships.ToListAsync();
+        }
     }
 }
