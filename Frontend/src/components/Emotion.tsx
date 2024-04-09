a24f55aa6c44c3a68279f70f0400fd2ec55122ba
Ewy Rostam <ewy.rost@gmail.com>
2024-04-09 15:32:04 +0200

    Make emotions button into resuable component

--- Frontend/src/components/Emotion.tsx
+++ Frontend/src/components/Emotion.tsx
@@ -1,5 +1,10 @@
+type Props = {
+    content: string;
+}
 
 
-export default function Emotion() {
-    <button className="border border-black rounded m-1 px-1 bg-green-400 hover:bg-green-500">{content}</button>
+export default function Emotion({content} : Props) {
+    return(
+        <button className="border border-black rounded m-1 px-1 bg-green-400 hover:bg-green-500">{content}</button>
+    )
 }
--- Frontend/src/routes/day.lazy.tsx
+++ Frontend/src/routes/day.lazy.tsx
@@ -1,6 +1,7 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import { month, today } from '../helpers/DateHelpers'
 import SearchBar from '../components/SearchBar'
+import RenderEmotions from '../components/RenderEmotions'
 
 export const Route = createLazyFileRoute('/day')({
   component: Day,
@@ -11,7 +12,8 @@ function Day() {
     <section className="flex flex-col items-center p-2 gap-2">
       <h1 className="text-3xl">{today} of {month}</h1>
       <h2 className="text-2xl">How did you feel today?</h2>
-      <SearchBar/>    
+      <SearchBar/>   
+      <RenderEmotions/>
     </section>
   )
 
