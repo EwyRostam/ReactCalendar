f7f60f4d2ab150ebe83b5cab0931a27775b1994a
Ewy Rostam <ewy.rost@gmail.com>
2024-04-09 22:09:29 +0200

    Update database relationship not required on day entity

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be0acb66335d3604015bee9f42c8f945d8014049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d907176c9a676285a5bc8386b4cc7911c8db504")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
