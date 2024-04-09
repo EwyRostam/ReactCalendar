b9c1803f74d8b6e44309edc81380808106c0867b
Ewy Rostam <ewy.rost@gmail.com>
2024-04-09 18:30:56 +0200

    Add emotions to selected emotions state onclick

--- Frontend/src/components/Emotion.tsx
+++ Frontend/src/components/Emotion.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import { Feeling } from "../api/EmotionsAPI";
 
 type Props = {
     feeling: Feeling;
 }
 
+export const [selectedEmotions, setSelectedEmotions] = useState<Feeling[]>([]);
 
 export default function Emotion({feeling} : Props) {
     const color = feeling.value > 0 ? "bg-green-400 hover:bg-green-500" : "bg-red-400 hover:bg-red-500";
+    const emotions: Feeling[] = [];
+
+    const handleClick = (feeling: Feeling) => {
+        emotions.push(feeling);
+        setSelectedEmotions(emotions);
+    }
 
     return(
-        <button  className={`border border-black rounded m-1 px-1 ${color}`}>{feeling.content}</button>
+        <button onClick={() => handleClick(feeling)} className={`border border-black rounded m-1 px-1 ${color}`}>{feeling.content}</button>
     )
 }
