58970801a9ef7bedd656122895fe47ccc242e06f
Ewy Rostam <ewy.rost@gmail.com>
2024-04-09 20:07:08 +0200

    Hide selected emotions for day after clicking them

--- Frontend/src/components/Emotion.tsx
+++ Frontend/src/components/Emotion.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Feeling } from "../api/EmotionsAPI";
 
 type Props = {
@@ -11,13 +12,15 @@ export default function Emotion({ feeling, setArr, selectedArr }: Props) {
 
     const color = feeling.value > 0 ? "bg-green-400 hover:bg-green-500" : "bg-red-400 hover:bg-red-500";
     const emotions: Feeling[] = selectedArr?.slice() ?? [];
+    const [visibility, setVisibility] = useState<string>("");
 
     const handleClick = (feeling: Feeling) => {
         emotions.push(feeling);
         setArr!(emotions);
+        setVisibility("invisible");
     }
 
     return (
-        <button onClick={() => handleClick(feeling)} className={`border border-black rounded m-1 px-1 ${color}`}>{feeling.content}</button>
+        <button onClick={() => handleClick(feeling)} className={`border border-black rounded m-1 px-1 ${color} ${visibility}`}>{feeling.content}</button>
     )
 }
