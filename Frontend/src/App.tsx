1ba9ffd9033e31df75f37e621fb7285599837bb1
Ewy Rostam <ewy.rost@gmail.com>
2024-04-11 01:48:32 +0200

    Add all pages in app

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import About from "./routes/about.lazy";
+import Day from "./routes/day.lazy";
+import Index from "./routes/index.lazy";
+import Overview from "./routes/overview.lazy";
 
 export default function App() {
   return (
     <>
+    <About/>
+    <Day/>
+    <Index/>
+    <Overview/>
     </>
   );
 }
