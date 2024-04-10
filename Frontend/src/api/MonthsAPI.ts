20baaf1488d13bc63a886f8e402ad1e250bd0561
Ewy Rostam <ewy.rost@gmail.com>
2024-04-10 19:56:16 +0200

    Fetch and deconstruct month

--- Frontend/src/api/MonthsAPI.ts
+++ Frontend/src/api/MonthsAPI.ts
@@ -2,12 +2,18 @@ import { DayType } from "./DaysAPI";
 
 const URL_BASE = "http://localhost:5236/Months";
 
-type Month = {
-    monthIndex: number
-    DaysInMonth: DayType[]
-}
+export type MonthReq = {
+  monthIndex: number;
+};
 
-export const getMonth = async ({monthIndex}: Month): Promise<Month> => {
-    const response = await fetch(URL_BASE + `/${monthIndex}`).then(result => result.json())
-    return response as Month
-}
+export type MonthRes = {
+  monthIndex: number;
+  daysInMonth: { $values: DayType[] };
+};
+
+export const getSpecificMonth = async ({monthIndex}: MonthReq): Promise<MonthRes> => {
+  const response = await fetch(URL_BASE + `/${monthIndex}`).then((result) =>
+    result.json()
+  );
+  return response as MonthRes;
+};
--- Frontend/src/components/EmotionChart.tsx
+++ Frontend/src/components/EmotionChart.tsx
@@ -1,5 +1,7 @@
-import { PureComponent } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { MonthReq, MonthRes, getSpecificMonth } from '../api/MonthsAPI';
+import { monthAsNumber } from '../helpers/DateHelpers';
+import { useQuery } from 'react-query';
 
 type DayData = {
   name: number;
@@ -39,33 +41,45 @@ const data = [
   },
 ];
 
-export default class EmotionChart extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/simple-line-chart-kec3v';
+export default function EmotionChart() {
+  const monthIndex: number = monthAsNumber + 1;
+  const month: MonthReq = { monthIndex }
 
-  render() {
-    return (
-      <section className="h-80">
-        <ResponsiveContainer width="100%" height="100%">
-          <LineChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="MoodIndex" stroke="#8884d8" activeDot={{ r: 8 }} />
-          </LineChart>
-        </ResponsiveContainer>
-      </section>
-    );
+  const fetchedMonth = async () => {
+    const result = await getSpecificMonth(month)
+    const { daysInMonth } = result as MonthRes
+    const { $values } = daysInMonth
+    return $values
   }
+
+  const daysArray = useQuery('month', fetchedMonth).data
+
+
+
+
+  return (
+    <section className="h-80">
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="MoodIndex" stroke="#8884d8" activeDot={{ r: 8 }} />
+        </LineChart>
+      </ResponsiveContainer>
+    </section>
+  );
+
 }
