8023aa09fdd3bd2c5f3eba0283a6a54f5150de8b
Ewy Rostam <ewy.rost@gmail.com>
2024-04-12 00:03:49 +0200

    Merge with developing branch

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4436539bd7aed79c22bacbe92d6e645805d7011")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92ad1e268b09ab6dcfb30cf0ec5b0c90d83a3afe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
