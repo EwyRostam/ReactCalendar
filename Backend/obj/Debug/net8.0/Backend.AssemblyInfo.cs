80e77b245cd874ddaf1be8f1686511203fc536e7
Ewy Rostam <ewy.rost@gmail.com>
2024-04-12 10:00:14 +0200

    Working to fetch days

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aae2a0ee063374e0fa882411f64421b68e1295c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d876b3bc6d8bd0e23bd27e9bd93cb8e274e6c15")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
