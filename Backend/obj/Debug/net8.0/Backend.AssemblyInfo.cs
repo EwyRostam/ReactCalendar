d89061d966fff4034310c58db695ea423293cd36
Ewy Rostam <ewy.rost@gmail.com>
2024-04-14 00:13:27 +0200

    Assemblyinfo updated

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6107ef09435eab1f727f785e0a80650f2f14dcf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f47e5afd7c59a5dbc49dc37fd8a8d2f1661d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
