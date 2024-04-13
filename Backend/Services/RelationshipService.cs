4c7fcf85e4bad4e830298b540c4dc6b6f1a842d1
Ewy Rostam <ewy.rost@gmail.com>
2024-04-13 23:27:47 +0200

    Move out delete relationship to separate function

--- Backend/Services/RelationshipService.cs
+++ Backend/Services/RelationshipService.cs
@@ -50,5 +50,15 @@ namespace Backend.Services
         {
             return await _repo.GetAllAsync();
         }
+
+        public async Task<bool> DeleteRelationshipAsync(int id)
+        {
+            if(await _repo.ExistsAsync(rel => rel.Id == id))
+            {
+                await _repo.DeleteAsync(rel => rel.Id == id);
+                return true;
+            }
+            return false;
+        }
     }
 }
