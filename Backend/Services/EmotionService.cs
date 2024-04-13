f7081a4612031945dc5ba671209541cdd09033aa
Ewy Rostam <ewy.rost@gmail.com>
2024-04-13 23:05:12 +0200

    Move out create emotion to separate service

---
+++ Backend/Services/EmotionService.cs
@@ -0,0 +1,49 @@
+using Backend.Models.DTOs;
+using Backend.Models.Enteties;
+using static Backend.Repositories.Repositories;
+
+namespace Backend.Services
+{
+    public class EmotionService
+    {
+        private readonly EmotionRepo _repo;
+
+        public EmotionService(EmotionRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<Emotion> CreateEmotionAsync(EmotionRequest emotionReq)
+        {
+            
+            var oppositeEmotion = await _repo.GetSpecificAsync(feeling => feeling.Content == emotionReq.Opposite);
+
+            Emotion emotion = new Emotion()
+            {
+                Content = emotionReq.Content,
+                Value = emotionReq.Value,
+                Opposite = ""
+            };
+
+            if (oppositeEmotion == null)
+            {
+                oppositeEmotion = new Emotion()
+                {
+                    Content = emotionReq.Opposite,
+                    Value = emotionReq.Value > 0 ? -1 : 1,
+                    Opposite = emotion.Content,
+                };
+                await _repo.CreateAsync(oppositeEmotion);
+            }
+            emotion.Opposite = oppositeEmotion.Content;
+
+            return emotion;
+       
+        }
+
+        public async Task<bool> EmotionExistsAsync(EmotionRequest emotionReq)
+        {
+            return await _repo.ExistsAsync(feeling => feeling.Content == emotionReq.Content);
+        }
+    }
+}
