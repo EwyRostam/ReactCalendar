b9373d396ab9d2e405f28d3d8d07f8fcf9e71d5c
Ewy Rostam <ewy.rost@gmail.com>
2024-04-13 15:57:59 +0200

    Move create day into service

---
+++ Backend/Services/DayService.cs
@@ -0,0 +1,55 @@
+
+using Backend.Models.DTOs;
+using Backend.Models.Enteties;
+using static Backend.Repositories.Repositories;
+
+namespace Backend.Services
+{
+    public class DayService
+    {
+        private readonly DayRepo _repo;
+
+        public DayService(DayRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<Day> CreateDayAsync(DayRequest dayReq)
+        {
+            var score = 0;
+            dayReq.Emotions!.ForEach(feeling => score += feeling.Value);
+
+            var allEmotions = await EmotionService.GetAllAsync();
+
+            var emotions = new List<Emotion>();
+            dayReq.Emotions.ForEach(feeling => emotions.Add(
+                allEmotions.First(e =>
+                e.Content == feeling.Content)
+            ));
+
+            var dayToAdd = new Day()
+            {
+                Date = dayReq.Date,
+                Month = dayReq.Month,
+                Emotions = emotions,
+                Score = score
+            };
+            var month = await MonthService.GetSpecificAsyn(dayToAdd.Month);
+
+
+            if (month == null)
+            {
+                month = new Month()
+                {
+                    MonthIndex = dayToAdd.Month
+                };
+                await MonthService.CreateMonthAsync(month);
+            }
+
+
+            month.DaysInMonth.Add(dayToAdd);
+
+            return dayToAdd;
+        }
+    }
+}
