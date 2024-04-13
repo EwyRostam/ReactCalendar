e3af45d3ce61c7f971539f283951362d8aa9f2b0
Ewy Rostam <ewy.rost@gmail.com>
2024-04-13 15:30:15 +0200

    Create override function for get specific day

---
+++ Backend/Repositories/Repositories.cs
@@ -0,0 +1,31 @@
+using System.Linq.Expressions;
+using Backend.Data;
+using Backend.Models.Enteties;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Repositories
+{
+    public class Repositories
+    {
+
+        public class DayRepo : Repo<Day>
+        {
+            private AppDBContext _context;
+
+            public DayRepo(AppDBContext context) : base(context)
+            {
+                _context = context;
+            }
+
+            public override async Task<Day> GetSpecificAsync(Expression<Func<Day, bool>> predicate)
+            {
+                return await _context.Days
+                .Include(day => day.Relationship)
+                .Include(day => day.Emotions)
+                .FirstOrDefaultAsync(predicate) ?? null!;
+                
+            }
+        }
+        
+    }
+}
