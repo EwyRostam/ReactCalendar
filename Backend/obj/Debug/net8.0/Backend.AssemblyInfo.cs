059b05a7659fe457221693bf12c8dadf10f6222f
Ewy Rostam <ewy.rost@gmail.com>
2024-04-14 22:18:18 +0200

    Remove weather forecast

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+272f6693a88d8b45092da0b1c4a239b2cd121f4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21290bf6304be6a13e53530a0ad1604ad5872730")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
