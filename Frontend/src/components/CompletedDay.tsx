c16012ca365ca9902ef9f7d9928c754c884da60c
Ewy Rostam <ewy.rost@gmail.com>
2024-04-14 23:16:16 +0200

    Change conditions in completed day so that correct view is shown when clicking date

--- Frontend/src/components/CompletedDay.tsx
+++ Frontend/src/components/CompletedDay.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import { DayReq, DayRes, getSpecificDay } from "../api/DaysAPI";
 import RenderEmotions from "./RenderEmotions";
 import DayComponent from "./DayComponent";
+import { Feeling } from "../api/EmotionsAPI";
 
 type Props = {
     date: number;
@@ -22,22 +23,26 @@ export default function CompletedDay({ date, month }: Props) {
 
     const { data: result, isError, isLoading } = useQuery(['day', window.location.href], fetchedDay);
 
-   
-
+    let emotionsList : Feeling[] = [];
     if (result) {
         const { emotions } = result as DayRes;
         const { $values } = emotions;
+        emotionsList = $values;
+    }
+
+    if (emotionsList.length > 0) {
         return (
             <section className="flex flex-col items-center p-2 gap-2">
+                {isError && <p>Oooops, an error occured!</p>}
                 {isLoading && <p>Loading...</p>}
-                {$values.length > 0 && <><h1 className="text-3xl">Your feelings {date}/{month}</h1>
+                {emotionsList.length > 0 && <><h1 className="text-3xl">Your feelings {date}/{month}</h1>
                     <article className="border border-black rounded-md w-60 h-48">
-                        <RenderEmotions emotions={$values ?? []} />
+                        <RenderEmotions emotions={emotionsList ?? []} />
                     </article>
                 </>}
-                {result.content &&
+                {result!.content &&
                     <article className="border border-black rounded-md w-60 h-48 p-2">
-                        {result.content}
+                        {result!.content}
                     </article>}
             </section>
         )
