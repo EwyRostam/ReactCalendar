3d39f48a20e8d2966d8284d0d54b3cb522e47a6a
Ewy Rostam <ewy.rost@gmail.com>
2024-04-15 20:56:22 +0200

    Create component for relationship card

--- Frontend/src/routes/index.lazy.tsx
+++ Frontend/src/routes/index.lazy.tsx
@@ -3,6 +3,11 @@ import { monthAsNumber, today } from '../helpers/DateHelpers';
 import { Calendar } from '../components/Calendar'
 import { DayReq } from '../api/daysAPI/Types';
 import { useGetSpecificDay } from '../hooks/useGetSpecificDay';
+import { useState } from 'react';
+import { Relationship } from '../api/relationshipsAPI/Types';
+import { getAllRelationships, getSpecificRelationship } from '../api/relationshipsAPI/RelationshipsAPI';
+import { useQuery } from 'react-query';
+import RelCard from '../components/RelCard';
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
@@ -11,36 +16,38 @@ export const Route = createLazyFileRoute('/')({
 
 
 export default function Index() {
-  // const [selectedRelationship, setSelectedRelationship] = useState<Relationship>();
+  const [selectedRelationship, setSelectedRelationship] = useState<Relationship>();
 
 
-  // const stored = sessionStorage.getItem("storedRelationship");
-  // if (stored) {
-  //   const keyAsInt = parseInt(stored);
-  //   const restoreSelected = async () => {
-  //     const result = await getSpecificRelationship(keyAsInt)
-  //     setSelectedRelationship(result);
-  //   }
-  //   restoreSelected();
-  // }
+  const stored = sessionStorage.getItem("storedRelationship");
+  if (stored) {
+    const keyAsInt = parseInt(stored);
+    const restoreSelected = async () => {
+      const result = await getSpecificRelationship(keyAsInt)
+      setSelectedRelationship(result);
+    }
+    restoreSelected();
+  }
 
-  // const handleClick = (rel: Relationship) => {
-  //   setSelectedRelationship(rel);
-  //   sessionStorage.setItem("storedRelationship", `${rel.id}`)
-  // }
-  // const { data, isLoading, isError } = useQuery('relationships', getAllRelationships);
+  const handleClick = (rel: Relationship) => {
+    setSelectedRelationship(rel);
+    sessionStorage.setItem("storedRelationship", `${rel.id}`)
+  }
+  const { data, isLoading, isError } = useQuery('relationships', getAllRelationships);
 
-  // if (selectedRelationship == null) {
-  //   console.log(data)
-  //   return (
-  //     <div className="h-screen flex flex-col items-center gap-4 lg:flex-row lg:justify-center">
-  //       {isLoading && <p>Loading...</p>}
-  //       {isError && <p>An error ocurred...</p>}
-  //       {data && data.map(rel =>
-  //         <Card rel={rel} key={rel.id} handleClick={handleClick} />)}
-  //     </div>
-  //   )
-  // }
+  if (selectedRelationship == null) {
+    console.log(data)
+    return (
+      <div className="h-screen flex flex-col items-center gap-4 lg:flex-row lg:justify-center">
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>An error ocurred...</p>}
+        {data && data.map(rel =>
+          <RelCard rel={rel} key={rel.id} handleClick={handleClick} />)}
+      </div>
+    )
+  }
+
+
   const date = today;
   const month = monthAsNumber
   const dayReq: DayReq = { date, month };
