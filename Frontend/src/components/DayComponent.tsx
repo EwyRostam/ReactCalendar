5d6c867edbb2d888d1d9407bb5d0fc00de4c6417
Ewy Rostam <ewy.rost@gmail.com>
2024-04-15 19:59:32 +0200

    Use Card component in day component

--- Frontend/src/components/DayComponent.tsx
+++ Frontend/src/components/DayComponent.tsx
@@ -11,6 +11,7 @@ import { Feeling } from '../api/emotoinsAPI/Types'
 import { createDay } from '../api/daysAPI/DaysAPI'
 import { DayType } from '../api/daysAPI/Types'
 import { useMutation, useQueryClient } from 'react-query'
+import Card from './Card'
 
 type Props = {
     date: number;
@@ -43,7 +44,7 @@ export default function DayComponent({ date, month }: Props) {
             return createDay(day);
         },
         onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: ['days']})
+            queryClient.invalidateQueries({ queryKey: ['days'] })
         },
     })
 
@@ -67,48 +68,46 @@ export default function DayComponent({ date, month }: Props) {
 
     const emotions = selectedEmotions;
 
-    const dayToCreate: DayType = { date, month, emotions, content};
+    const dayToCreate: DayType = { date, month, emotions, content };
 
-    const heading = format(new Date(year, month -1, date), 'PPP')
+    const heading = format(new Date(year, month - 1, date), 'PPP')
 
     if (showModal) {
         return (
-            <AddEmotions handleClick={toggleModal}/>
+            <AddEmotions handleClick={toggleModal} />
         )
     }
 
 
     return (
+        <>
 
-        <section className="flex justify-center bg-background">
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <div className="card-body">
-                    <div className="card-actions justify-end">
-                        <Link to="/" className="btn btn-square btn-sm" onClick={() => hideDayView()}>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                        </Link>
-                    </div>
+            <Card
+                button={<Link to="/" className="btn btn-square btn-sm" onClick={() => hideDayView()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </Link>}
+                content={
+                    <>
+                        <section className="flex flex-col items-center p-2 gap-2">
+                            <h1 className="text-3xl">{heading}</h1>
+                            <h2 className="text-2xl">How did you feel this day?</h2>
+                            <SearchBar setSearchInput={setSearchInput} searchInput={searchInput} />
+                            <article className="border border-black rounded-md w-60 h-48">
+                                <RenderEmotions handleClick={onClickSelected} emotions={selectedEmotions} />
+                            </article>
+                            <textarea ref={contentRef} onChange={() => setContent(contentRef.current!.value)}
+                                className="border border-black rounded-md w-60 h-48 p-2">
 
-                    <section className="flex flex-col items-center p-2 gap-2">
-                        <h1 className="text-3xl">{heading}</h1>
-                        <h2 className="text-2xl">How did you feel this day?</h2>
-                        <SearchBar setSearchInput={setSearchInput} searchInput={searchInput} />
-                        <article className="border border-black rounded-md w-60 h-48">
-                            <RenderEmotions handleClick={onClickSelected} emotions={selectedEmotions} />
-                        </article>
-                        <textarea ref={contentRef} onChange={() => setContent(contentRef.current!.value)}
-                        className="border border-black rounded-md w-60 h-48 p-2">
-
-                        </textarea>
-                        <Link to="/" onClick={() => onCreate.mutate(dayToCreate)} className="btn btn-outline btn-success w-60">
-                            Submit
-                        </Link>
-                        <RenderEmotions handleClick={onClickAll} emotions={filteredEmotions ?? []} />
-                    </section>
-                    <SquareBtn handleClick={toggleModal} />
-
-                </div>
-            </div>
-        </section>
+                            </textarea>
+                            <Link to="/" onClick={() => onCreate.mutate(dayToCreate)} className="btn btn-outline btn-success w-60">
+                                Submit
+                            </Link>
+                            <RenderEmotions handleClick={onClickAll} emotions={filteredEmotions ?? []} />
+                        </section>
+                        <SquareBtn handleClick={toggleModal} />
+                    </>
+                }
+            />
+        </>
     )
 }
