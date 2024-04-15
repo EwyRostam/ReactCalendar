5950f87c417fbff4249e2ddd8528c06fa382d410
Ewy Rostam <ewy.rost@gmail.com>
2024-04-15 20:04:31 +0200

    Add linebreaks

--- Frontend/src/components/DayComponent.tsx
+++ Frontend/src/components/DayComponent.tsx
@@ -83,9 +83,11 @@ export default function DayComponent({ date, month }: Props) {
         <>
 
             <Card
-                button={<Link to="/" className="btn btn-square btn-sm" onClick={() => hideDayView()}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                </Link>}
+                button={
+                    <Link to="/" className="btn btn-square btn-sm" onClick={() => hideDayView()}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                    </Link>
+                }
                 content={
                     <>
                         <section className="flex flex-col items-center p-2 gap-2">
