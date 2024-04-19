16571c93292077b72c21a2e61e6f5554d8bf2028
Ewy Rostam <ewy.rost@gmail.com>
2024-04-19 10:55:00 +0200

    Include months in relationship type

--- Frontend/src/api/relationshipsAPI/Types.ts
+++ Frontend/src/api/relationshipsAPI/Types.ts
@@ -1,8 +1,10 @@
 import { Feeling } from "../emotoinsAPI/Types";
+import { MonthRes } from "../monthsAPI/Types";
 
 export type Relationship = {
     id?: number;
     name?: string;
     category?: string;
+    months: MonthRes[];
     wantedEmotions?: Feeling[];
 }
