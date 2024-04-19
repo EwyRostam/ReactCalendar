1fd40761037c2da1747bb95482a1071f78e4c8d5
Ewy Rostam <ewy.rost@gmail.com>
2024-04-19 17:22:16 +0200

    Reload page when seeing single day view

--- Frontend/src/hooks/useGetSpecificDay.ts
+++ Frontend/src/hooks/useGetSpecificDay.ts
@@ -4,12 +4,13 @@ import { DayReq } from "../api/daysAPI/Types";
 
 const fetchedDay = async (dayReq : DayReq) => {
     const result = await getSpecificDay(dayReq)
+    window.location.href;
     return result
 }
 
 export const useGetSpecificDay = (dayReq : DayReq) => { 
     return useQuery({
-    queryKey: ['day', window.location.href], 
+    queryKey: ['day'], 
     queryFn: () => fetchedDay(dayReq)}
 )
 }
