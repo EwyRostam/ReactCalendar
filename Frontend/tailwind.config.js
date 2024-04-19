7c8ac58a6d5e781794b26c0c3ad04d2d79d77564
Ewy Rostam <ewy.rost@gmail.com>
2024-04-19 21:22:34 +0200

    Add custom palette colors

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -7,7 +7,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: '#fdfdfd'
+        background: '#fdfdfd',
+        'positive': {
+          50: '#DEF8D6',
+          100: '#A1C398',
+          200: '#799A71',
+          300: '#54734C',
+          400: '#304E2A',
+          500: '#0E2C09'
+        },
+        'negative': {
+          50: '#FFE4E0',
+          100: '#FA7070',
+          200: '#D24D51',
+          300: '#AB2834',
+          400: '#840019',
+          500: '#5F0000'
+
+        }
       },
     },
   },
