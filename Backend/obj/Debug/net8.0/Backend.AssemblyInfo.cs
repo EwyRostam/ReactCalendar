16bda98e55c3febf15c2b292eaa93c4df8f27615
Ewy Rostam <ewy.rost@gmail.com>
2024-04-20 10:56:35 +0200

    Add color when score is zero

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db886136e949d5222ed7b3f6555c3de6971223d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e61d8566d403a31ffea098d1e9cafc39e6093a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
