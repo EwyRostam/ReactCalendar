72e20b56d4c3469a9232f1dedd24df19244de83e
Ewy Rostam <ewy.rost@gmail.com>
2024-04-21 21:30:08 +0200

    Add number of days to month request

--- Frontend/src/api/monthsAPI/Types.ts
+++ Frontend/src/api/monthsAPI/Types.ts
@@ -2,6 +2,7 @@ import { DayData } from "../../components/EmotionChart";
 
 export type MonthReq = {
     monthIndex: number;
+    numberOfDays: number;
   };
   
   export type MonthRes = {
--- Frontend/src/routes/index.lazy.tsx
+++ Frontend/src/routes/index.lazy.tsx
@@ -4,6 +4,8 @@ import { Calendar } from '../components/Calendar'
 import { DayReq } from '../api/daysAPI/Types';
 import { useGetSpecificDay } from '../hooks/useGetSpecificDay';
 import { getEmotionsList } from '../helpers/FilterDayHelpers';
+import { createMonth } from '../api/monthsAPI/MonthsAPI';
+import { MonthReq } from '../api/monthsAPI/Types';
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
@@ -16,6 +18,9 @@ export default function Index() {
   const date = today;
   const month = monthAsNumber
   const dayReq: DayReq = { date, month };
+  const monthReq: MonthReq = {monthIndex: month, numberOfDays};
+
+  date == 1 ? createMonth(monthReq) :
 
   const { data: day } = useGetSpecificDay(dayReq);
   const emotionsList = getEmotionsList(day);
